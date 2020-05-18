52e4eab0a5b6d59a01e01333cc8752ad27113826
JEFFGULICK <jeffgulick@gmail.com>
2020-05-17 23:52:54 -0500

    a few last minute changes to some pic, the logo, and animations

--- animations.js
+++ animations.js
@@ -1,9 +1,9 @@
 const flip = () => {
     const x = document.getElementById("logo");
-    x.classList.add('animate__animated', 'animate__flip');
+    x.classList.add('animate__animated', 'animate__bounce');
     //This removes the class "animate" and sets back to default class//
     x.addEventListener('animationend', () => {
-        x.classList.remove('animate__animated', 'animate__flip')
+        x.classList.remove('animate__animated', 'animate__bounce')
       });
   }
   
